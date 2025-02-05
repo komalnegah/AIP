27eb4280d5a87a0b436b50bd2e42e2de982bdd48
komal negah <komalnegah@loyalistcollege.com>
2025-02-05 09:51:06 -0500

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"; // Import Navigate here
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 import Dashboard from "./pages/Dashboard";
 import './components/Chatbox.css';
 
 function App() {
   return (
-    <div className="app">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/login" />} /> {/* Navigate is now imported */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
